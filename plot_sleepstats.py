9cfc106cfd1ab16031bc8496998d34903806e5d5
remrama <mallett.remy@gmail.com>
2023-03-18 20:12:39 -0600

    draft plot sleepstats

---
+++ plot_sleepstats.py
@@ -0,0 +1,45 @@
+"""Plot summary sleep statistics across all participants."""
+import argparse
+from pathlib import Path
+
+from bids import BIDSLayout
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+import utils
+
+
+bids_root = Path(utils.config["bids_root"])
+export_path = bids_root / "derivatives" / "sleepstats.png"
+
+layout = BIDSLayout(bids_root, validate=False, derivatives=True)
+
+bids_files = layout.get(task="sleep", suffix="sstats", extension="tsv")
+
+df = pd.concat(
+    [bf.get_df().assign(participant_id="sub-" + str(bf.entities["subject"])) for bf in bids_files],
+    ignore_index=True,
+)
+
+
+palette = utils.load_participant_palette()
+
+g = sns.catplot(
+    kind="bar",
+    data=df,
+    col="sleep_statistic",
+    x="sleep_statistic",
+    y="value",
+    color="gainsboro",
+    col_wrap=7,
+    height=1,
+    aspect=1,
+    sharex=False, sharey=False,
+)
+g.set_titles("")
+g.set_xlabels("")
+
+
+# Export.
+utils.export_mpl(export_path)
